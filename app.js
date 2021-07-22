21ed35e2bbd3324d548b46b93fd8e61fd70d6228
fatih <fatihenaz@gmail.com>
2021-07-22 15:18:08 +0300

    functions added

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ let screen = document.querySelector(".result")
 let operatorButton = document.querySelectorAll(".operator")
 let selectedOperator = "" // seçileck operatörü burada tutuyoruz.
 // İşlem yapabilmek için numaraları da bir yerde tutmamız gerek.
-// Bunu da iki parça olarak düşünelim. opertorden önceki ve sonraki şeklinde
+// Bunu da iki parça olarak düşünelim. operatorden önceki ve sonraki şeklinde
 let leftPart = "";
 let rightPart = "";
 
 
 
-
+// number class buttons
 numberButton.forEach(number => {
     number.addEventListener("click", displayNum)
     function displayNum() {
@@ -17,8 +17,6 @@ numberButton.forEach(number => {
             screen.innerHTML = ""
         }
         screen.innerHTML += this.textContent
-        // leftPart += this.value
-
         if (selectedOperator == "") {
           leftPart += this.value
         } else if(selectedOperator != "" ){
@@ -27,6 +25,8 @@ numberButton.forEach(number => {
     }
 });
 
+
+// operator buttons
 operatorButton.forEach(operator => {
     operator.addEventListener("click", compute)
     function compute() {
@@ -36,22 +36,13 @@ operatorButton.forEach(operator => {
 
 });
 
+// (=) equal button
 document.querySelector("#calculate").addEventListener("click", function () {
     // = bastığımızda sonucu görmek için;
-    if (selectedOperator == "+") {
-        screen.innerHTML = parseInt(leftPart) + parseInt(rightPart);
-        // var add = parseInt(leftPart) + parseInt(rightPart);
-        // document.querySelector(".result").innerHTML = add
-    } else if (selectedOperator == "-") {
-        screen.innerHTML = leftPart - rightPart;
-    } else if (selectedOperator == "*") {
-        screen.innerHTML = leftPart * rightPart
-    } else if (selectedOperator == "÷") {
-        screen.innerHTML = leftPart / rightPart
-    }
+    screen.textContent = eval(screen.textContent)
 });
 
-
+// AC button
 document.querySelector(".ac").addEventListener("click", function () {
     selectedOperator = ""
     leftPart = ""
@@ -60,18 +51,79 @@ document.querySelector(".ac").addEventListener("click", function () {
 })
 
 
-
+// % (percent) button
 document.querySelector(".mod").addEventListener("click", function () {
-    let buttonClicked = this.innerHTML;
-    screen.innerHTML = buttonClicked;
-    if (buttonClicked == "%") {
-        screen.innerHTML = leftPart / 100
-    }
+    screen.innerHTML = eval(screen.innerHTML);
 })
 
+
+// plus minus button
 document.querySelector(".pm").addEventListener("click", function () {
     let buttonClicked = this.innerHTML;
     screen.innerHTML = buttonClicked;
     if (buttonClicked == "±")
-        screen.innerHTML = leftPart * -1;
+        screen.innerHTML = Number(leftPart * -1) || Number(rightPart * -1)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numberButton = document.querySelectorAll(".number")
+// let screen = document.querySelector(".result")
+// let operatorButton = document.querySelectorAll(".operator")
+
+// numberButton.forEach(number => {
+//     number.addEventListener("click", displayNum)
+//     function displayNum() {
+//         if (screen.innerHTML == "0") {
+//             screen.innerHTML = "";
+//         }
+
+//         screen.innerHTML += this.textContent
+//     }
+// });
+
+// operatorButton.forEach(operator => {
+//     operator.addEventListener("click", compute)
+//     function compute() {
+//         screen.innerHTML += operator.innerHTML
+//     }
+// })
+
+// operatorButton.forEach(function (operator){
+//     operator.addEventListener("click", () => {
+//         screen.innerHTML += operator.innerHTML;
+//     })
+// })
+
+
+// operatorButton.forEach(operator => {
+//     operator.addEventListener("click", compute)
+//     function compute() {
+//         screen.innerHTML += this.value  
+// }
+
+// document.querySelector(".ac").addEventListener("click", function () {
+//      screen.innerHTML = "0"
+// })
+ 
+// document.querySelector("#calculate").addEventListener("click", function () {
+//     screen.innerHTML = eval(screen.innerHTML)
+// })
